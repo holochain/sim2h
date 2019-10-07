137e13dfcd30bf5d9cf4bd6c1bb4f688978cad2e
Eric Harris-Braun <eric@harris-braun.com>
2019-10-07 10:12:42 -0400

    serialzie and deserialize of wire message

--- crates/sim2h/src/wire_message.rs
+++ crates/sim2h/src/wire_message.rs
@@ -1,6 +1,7 @@
 //! encapsulates lib3h ghostmessage for sim2h including security challenge
-use lib3h_protocol::protocol::*;
 use lib3h_protocol::data_types::Opaque;
+use lib3h_protocol::protocol::*;
+use std::convert::TryFrom;
 
 pub type Sim2hWireError = String;
 
@@ -18,16 +19,35 @@ pub enum WireMessage {
 
 impl From<WireMessage> for Opaque {
     fn from(message: WireMessage) -> Opaque {
-        serde_json::to_string(&message).expect("wiremessage should serialize").into()
+        serde_json::to_string(&message)
+            .expect("wiremessage should serialize")
+            .into()
+    }
+}
+
+impl TryFrom<Opaque> for WireMessage {
+    type Error = Sim2hWireError;
+    fn try_from(message: Opaque) -> Result<Self, Self::Error> {
+        Ok(serde_json::from_str(&String::from_utf8_lossy(&message))
+            .map_err(|e| format!("{:?}", e))?)
     }
 }
 
 #[cfg(test)]
 pub mod tests {
-    //use super::*;
+    use super::*;
+    //    use std::convert::Try;
 
     #[test]
     pub fn test_wire_message() {
-        assert!(true);
+        let msg = WireMessage::Err("fake_error".into());
+
+        let opaque_msg: Opaque = msg.clone().into();
+        assert_eq!(
+            "\"{\\\"Err\\\":\\\"fake_error\\\"}\"",
+            format!("{}", opaque_msg)
+        );
+        let roundtrip_msg = WireMessage::try_from(opaque_msg).expect("deserialize should work");
+        assert_eq!(roundtrip_msg, msg);
     }
 }
