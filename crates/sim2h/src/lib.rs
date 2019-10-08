3296e792a7fe12afbb9ceb75fc59e4b823a43f78
Nicolas Luck <nicolas@lucksus.eu>
2019-10-08 16:14:28 +0200

    Add structs Space and AspectList to prepare gossip/auth. list handling

--- crates/sim2h/src/lib.rs
+++ crates/sim2h/src/lib.rs
@@ -14,7 +14,7 @@ use crate::error::*;
 use detach::prelude::*;
 use holochain_tracing::Span;
 use lib3h::transport::protocol::*;
-use lib3h_protocol::{data_types::SpaceData, protocol::*, types::SpaceHash, uri::Lib3hUri};
+use lib3h_protocol::{data_types::SpaceData, protocol::*, types::SpaceHash, uri::Lib3hUri, Address};
 use lib3h_zombie_actor::prelude::*;
 
 use parking_lot::RwLock;
@@ -25,12 +25,107 @@ use lib3h_protocol::data_types::StoreEntryAspectData;
 use log::{debug, error, warn};
 use url::Url;
 pub use wire_message::WireMessage;
+use std::collections::HashSet;
+
+pub struct Space {
+    agents: HashMap<AgentId, Lib3hUri>,
+    all_aspects_hashes: AspectList,
+}
+
+impl Space {
+    pub fn new() -> Self {
+        Space {
+            agents: HashMap::new(),
+            all_aspects_hashes: AspectList::from(HashMap::new()),
+        }
+    }
+
+    pub fn join_agent(&mut self, agent_id: AgentId, uri: Lib3hUri) {
+        self.agents.insert(agent_id, uri);
+    }
+
+    pub fn remove_agent(&mut self, agent_id: &AgentId) {
+        self.agents.remove(agent_id);
+    }
+
+    pub fn agent_id_to_uri(&self, agent_id: &AgentId) -> Option<Lib3hUri> {
+        for (found_agent, uri) in self.agents.iter() {
+            if found_agent == agent_id {
+                return Some(uri.clone());
+            }
+        }
+        None
+    }
+
+    pub fn all_agents(&self) -> &HashMap<AgentId, Lib3hUri> {
+        &self.agents
+    }
+
+    pub fn add_aspect(&mut self, entry_address: Address, aspect_address: Address) {
+        self.all_aspects_hashes.add(entry_address, aspect_address);
+    }
+}
+
+pub struct AspectList(HashMap<Address, Vec<Address>>);
+impl AspectList {
+    /// Returns an AspectList list that contains every entry aspect
+    /// in self that is not in other.
+    pub fn diff(&self, other: &AspectList) -> AspectList {
+        let self_set = HashSet::<(Address, Address)>::from(self);
+        let other_set = HashSet::<(Address, Address)>::from(other);
+        AspectList::from(&self_set
+            .difference(&other_set)
+            .cloned()
+            .collect::<HashSet<(Address, Address)>>()
+        )
+    }
+
+    pub fn add(&mut self, entry_address: Address, aspect_address: Address) {
+        if !self.0.contains_key(&entry_address) {
+            self.0.insert(entry_address, vec![aspect_address]);
+        } else {
+            self.0.get_mut(&entry_address).unwrap().push(aspect_address);
+        }
+    }
+}
+
+impl From<HashMap<Address, Vec<Address>>> for AspectList {
+    fn from(map: HashMap<Address, Vec<Address>>) -> AspectList {
+        AspectList{0: map}
+    }
+}
+
+impl From<&AspectList> for HashSet<(Address, Address)> {
+    fn from(a: &AspectList) -> HashSet<(Address, Address)> {
+        let mut result = HashSet::new();
+        for (entry_address, aspect_list) in a.0.iter() {
+            for aspect_address in aspect_list {
+                result.insert((entry_address.clone(), aspect_address.clone()));
+            }
+        }
+        result
+    }
+}
+
+impl From<&HashSet<(Address, Address)>> for AspectList {
+    fn from(s: &HashSet<(Address, Address)>) -> AspectList {
+        let mut result: HashMap<Address, Vec<Address>> = HashMap::new();
+        for (entry_address, aspect_address) in s {
+            if !result.contains_key(entry_address) {
+                result.insert(entry_address.clone(), Vec::new());
+            }
+            result.get_mut(entry_address).unwrap().push(aspect_address.clone());
+
+        }
+        AspectList::from(result)
+    }
+}
 
 #[allow(dead_code)]
 pub struct Sim2h {
     pub bound_uri: Option<Lib3hUri>,
     connection_states: RwLock<HashMap<Lib3hUri, ConnectedAgent>>,
-    spaces: HashMap<SpaceHash, RwLock<HashMap<AgentId, Lib3hUri>>>,
+    spaces: HashMap<SpaceHash, RwLock<Space>>,
     transport: Detach<TransportActorParentWrapperDyn<Self>>,
 }
 
@@ -126,13 +221,13 @@ impl Sim2h {
             );
             if !self.spaces.contains_key(&data.space_address) {
                 self.spaces
-                    .insert(data.space_address.clone(), RwLock::new(HashMap::new()));
+                    .insert(data.space_address.clone(), RwLock::new(Space::new()));
             }
             self.spaces
                 .get(&data.space_address)
                 .unwrap()
                 .write()
-                .insert(data.agent_id.clone(), uri.clone());
+                .join_agent(data.agent_id.clone(), uri.clone());
             debug!(
                 "Agent {:?} joined space {:?}",
                 data.agent_id, data.space_address
@@ -155,7 +250,7 @@ impl Sim2h {
                     .get(&data.space_address)
                     .unwrap()
                     .write()
-                    .remove(&data.agent_id);
+                    .remove_agent(&data.agent_id);
                 Ok(())
             }
         } else {
@@ -171,12 +266,7 @@ impl Sim2h {
 
     // find out if an agent is in a space or not and return its URI
     fn lookup_joined(&self, space_address: &SpaceHash, agent_id: &AgentId) -> Option<Lib3hUri> {
-        for (found_agent, uri) in self.spaces.get(&space_address)?.read().iter() {
-            if found_agent == agent_id {
-                return Some(uri.clone());
-            }
-        }
-        None
+        self.spaces.get(&space_address)?.read().agent_id_to_uri(agent_id)
     }
 
     // handler for incoming connections
@@ -338,6 +428,10 @@ impl Sim2h {
                     return Err(SPACE_MISMATCH_ERR_STR.into());
                 }
                 debug!("Got Publish - broadcasting all aspects to all agents...");
+                //let space_lock = self.spaces
+                //    .get(space_address)
+                //    .expect("This function should not be called if we don't have this space");
+
                 for aspect in data.entry.aspect_list {
                     let store_message = WireMessage::Lib3hToClient(
                         Lib3hToClient::HandleStoreEntryAspect(StoreEntryAspectData {
@@ -441,6 +535,7 @@ impl Sim2h {
             .get(&space)
             .ok_or("No such space")?
             .read()
+            .all_agents()
             .values()
             .cloned()
         {
