6e2ee9fc77b41296e5f1e0d472e091a16c17110a
Eric Harris-Braun <eric@harris-braun.com>
2019-10-08 17:50:23 -0400

    move cache structures to their own mod

---
+++ crates/sim2h/src/cache.rs
@@ -0,0 +1,133 @@
+//! implements caching structures for spaces and aspects
+use crate::AgentId;
+use lib3h_protocol::{uri::Lib3hUri, Address};
+use std::collections::{HashMap, HashSet};
+
+pub struct Space {
+    agents: HashMap<AgentId, Lib3hUri>,
+    all_aspects_hashes: AspectList,
+}
+
+impl Space {
+    pub fn new() -> Self {
+        Space {
+            agents: HashMap::new(),
+            all_aspects_hashes: AspectList::from(HashMap::new()),
+        }
+    }
+
+    pub fn join_agent(&mut self, agent_id: AgentId, uri: Lib3hUri) {
+        self.agents.insert(agent_id, uri);
+    }
+
+    pub fn remove_agent(&mut self, agent_id: &AgentId) {
+        self.agents.remove(agent_id);
+    }
+
+    pub fn agent_id_to_uri(&self, agent_id: &AgentId) -> Option<Lib3hUri> {
+        for (found_agent, uri) in self.agents.iter() {
+            if found_agent == agent_id {
+                return Some(uri.clone());
+            }
+        }
+        None
+    }
+
+    pub fn all_agents(&self) -> &HashMap<AgentId, Lib3hUri> {
+        &self.agents
+    }
+
+    pub fn all_aspects(&self) -> &AspectList {
+        &self.all_aspects_hashes
+    }
+
+    pub fn add_aspect(&mut self, entry_address: Address, aspect_address: Address) {
+        self.all_aspects_hashes.add(entry_address, aspect_address);
+    }
+}
+
+#[derive(Debug)]
+pub struct AspectList(HashMap<Address, Vec<Address>>);
+impl AspectList {
+    /// Returns an AspectList list that contains every entry aspect
+    /// in self that is not in other.
+    pub fn diff(&self, other: &AspectList) -> AspectList {
+        let self_set = HashSet::<(Address, Address)>::from(self);
+        let other_set = HashSet::<(Address, Address)>::from(other);
+        AspectList::from(
+            &self_set
+                .difference(&other_set)
+                .cloned()
+                .collect::<HashSet<(Address, Address)>>(),
+        )
+    }
+
+    pub fn add(&mut self, entry_address: Address, aspect_address: Address) {
+        if !self.0.contains_key(&entry_address) {
+            self.0.insert(entry_address, vec![aspect_address]);
+        } else {
+            self.0.get_mut(&entry_address).unwrap().push(aspect_address);
+        }
+    }
+
+    pub fn entry_addresses(&self) -> impl Iterator<Item = &Address> {
+        self.0.keys()
+    }
+
+    pub fn per_entry(&self, entry_address: &Address) -> Option<&Vec<Address>> {
+        self.0.get(entry_address)
+    }
+
+    pub fn pretty_string(&self) -> String {
+        self.0
+            .iter()
+            .map(|(entry, aspects)| {
+                format!(
+                    "{}: [{}]",
+                    entry,
+                    aspects
+                        .iter()
+                        .cloned()
+                        .map(|i| i.into())
+                        .collect::<Vec<String>>()
+                        .join(", ")
+                )
+            })
+            .collect::<Vec<String>>()
+            .join("\n")
+    }
+}
+
+impl From<HashMap<Address, Vec<Address>>> for AspectList {
+    fn from(map: HashMap<Address, Vec<Address>>) -> AspectList {
+        AspectList { 0: map }
+    }
+}
+
+impl From<&AspectList> for HashSet<(Address, Address)> {
+    fn from(a: &AspectList) -> HashSet<(Address, Address)> {
+        let mut result = HashSet::new();
+        for (entry_address, aspect_list) in a.0.iter() {
+            for aspect_address in aspect_list {
+                result.insert((entry_address.clone(), aspect_address.clone()));
+            }
+        }
+        result
+    }
+}
+
+impl From<&HashSet<(Address, Address)>> for AspectList {
+    fn from(s: &HashSet<(Address, Address)>) -> AspectList {
+        let mut result: HashMap<Address, Vec<Address>> = HashMap::new();
+        for (entry_address, aspect_address) in s {
+            if !result.contains_key(entry_address) {
+                result.insert(entry_address.clone(), Vec::new());
+            }
+            result
+                .get_mut(entry_address)
+                .unwrap()
+                .push(aspect_address.clone());
+        }
+        AspectList::from(result)
+    }
+}
--- crates/sim2h/src/lib.rs
+++ crates/sim2h/src/lib.rs
@@ -6,11 +6,13 @@ extern crate detach;
 #[macro_use]
 extern crate serde;
 
+pub mod cache;
 pub mod connected_agent;
 pub mod error;
 pub mod wire_message;
 
 use crate::error::*;
+use cache::*;
 use connected_agent::*;
 pub use wire_message::WireMessage;
 
@@ -29,140 +31,10 @@ use lib3h_zombie_actor::prelude::*;
 use log::*;
 use parking_lot::RwLock;
 use std::{
-    collections::{HashMap, HashSet},
+    collections::HashMap,
     convert::TryFrom,
 };
 use rand::Rng;
-
-
-pub struct Space {
-    agents: HashMap<AgentId, Lib3hUri>,
-    all_aspects_hashes: AspectList,
-}
-
-impl Space {
-    pub fn new() -> Self {
-        Space {
-            agents: HashMap::new(),
-            all_aspects_hashes: AspectList::from(HashMap::new()),
-        }
-    }
-
-    pub fn join_agent(&mut self, agent_id: AgentId, uri: Lib3hUri) {
-        self.agents.insert(agent_id, uri);
-    }
-
-    pub fn remove_agent(&mut self, agent_id: &AgentId) {
-        self.agents.remove(agent_id);
-    }
-
-    pub fn agent_id_to_uri(&self, agent_id: &AgentId) -> Option<Lib3hUri> {
-        for (found_agent, uri) in self.agents.iter() {
-            if found_agent == agent_id {
-                return Some(uri.clone());
-            }
-        }
-        None
-    }
-
-    pub fn all_agents(&self) -> &HashMap<AgentId, Lib3hUri> {
-        &self.agents
-    }
-
-    pub fn all_aspects(&self) -> &AspectList {
-        &self.all_aspects_hashes
-    }
-
-    pub fn add_aspect(&mut self, entry_address: Address, aspect_address: Address) {
-        self.all_aspects_hashes.add(entry_address, aspect_address);
-    }
-}
-
-#[derive(Debug)]
-pub struct AspectList(HashMap<Address, Vec<Address>>);
-impl AspectList {
-    /// Returns an AspectList list that contains every entry aspect
-    /// in self that is not in other.
-    pub fn diff(&self, other: &AspectList) -> AspectList {
-        let self_set = HashSet::<(Address, Address)>::from(self);
-        let other_set = HashSet::<(Address, Address)>::from(other);
-        AspectList::from(
-            &self_set
-                .difference(&other_set)
-                .cloned()
-                .collect::<HashSet<(Address, Address)>>(),
-        )
-    }
-
-    pub fn add(&mut self, entry_address: Address, aspect_address: Address) {
-        if !self.0.contains_key(&entry_address) {
-            self.0.insert(entry_address, vec![aspect_address]);
-        } else {
-            self.0.get_mut(&entry_address).unwrap().push(aspect_address);
-        }
-    }
-
-    pub fn entry_addresses(&self) -> impl Iterator<Item = &Address> {
-        self.0.keys()
-    }
-
-    pub fn per_entry(&self, entry_address: &Address) -> Option<&Vec<Address>> {
-        self.0.get(entry_address)
-    }
-
-    pub fn pretty_string(&self) -> String {
-        self.0
-            .iter()
-            .map(|(entry, aspects)| {
-                format!(
-                    "{}: [{}]",
-                    entry,
-                    aspects
-                        .iter()
-                        .cloned()
-                        .map(|i| i.into())
-                        .collect::<Vec<String>>()
-                        .join(", ")
-                )
-            })
-            .collect::<Vec<String>>()
-            .join("\n")
-    }
-}
-
-impl From<HashMap<Address, Vec<Address>>> for AspectList {
-    fn from(map: HashMap<Address, Vec<Address>>) -> AspectList {
-        AspectList { 0: map }
-    }
-}
-
-impl From<&AspectList> for HashSet<(Address, Address)> {
-    fn from(a: &AspectList) -> HashSet<(Address, Address)> {
-        let mut result = HashSet::new();
-        for (entry_address, aspect_list) in a.0.iter() {
-            for aspect_address in aspect_list {
-                result.insert((entry_address.clone(), aspect_address.clone()));
-            }
-        }
-        result
-    }
-}
-
-impl From<&HashSet<(Address, Address)>> for AspectList {
-    fn from(s: &HashSet<(Address, Address)>) -> AspectList {
-        let mut result: HashMap<Address, Vec<Address>> = HashMap::new();
-        for (entry_address, aspect_address) in s {
-            if !result.contains_key(entry_address) {
-                result.insert(entry_address.clone(), Vec::new());
-            }
-            result
-                .get_mut(entry_address)
-                .unwrap()
-                .push(aspect_address.clone());
-        }
-        AspectList::from(result)
-    }
-}
 
 pub struct Sim2h {
     pub bound_uri: Option<Lib3hUri>,
@@ -646,7 +518,7 @@ impl Sim2h {
                 debug!(
                     "Space {} now knows about these aspects:\n{}",
                     space_address,
-                    space.all_aspects_hashes.pretty_string()
+                    space.all_aspects().pretty_string()
                 );
             }
 
@@ -1028,7 +900,7 @@ pub mod tests {
             .expect("there should be a server for to_uri");
         if let Ok((did_work, events)) = server.process() {
             assert!(did_work);
-            let dm = &events[2];
+            let dm = &events[3];
             assert_eq!(
                 "ReceivedData(Lib3hUri(\"mem://addr_3/\"), \"{\\\"Lib3hToClient\\\":{\\\"HandleSendDirectMessage\\\":{\\\"space_address\\\":\\\"fake_space_address\\\",\\\"request_id\\\":\\\"\\\",\\\"to_agent_id\\\":\\\"fake_to_agent_id\\\",\\\"from_agent_id\\\":\\\"fake_agent_id\\\",\\\"content\\\":\\\"Zm9v\\\"}}}\")",
                 format!("{:?}", dm))
@@ -1104,7 +976,7 @@ pub mod tests {
                 .expect("there should be a server for to_uri");
             if let Ok((did_work, events)) = server.process() {
                 assert!(did_work);
-                let dm = &events[2];
+                let dm = &events[3];
                 assert_eq!(
                    "ReceivedData(Lib3hUri(\"mem://addr_1/\"), \"{\\\"Lib3hToClient\\\":{\\\"HandleSendDirectMessage\\\":{\\\"space_address\\\":\\\"fake_space_address\\\",\\\"request_id\\\":\\\"\\\",\\\"to_agent_id\\\":\\\"agent2\\\",\\\"from_agent_id\\\":\\\"agent1\\\",\\\"content\\\":\\\"Y29tZSBoZXJlIHdhdHNvbg==\\\"}}}\")",
                     format!("{:?}", dm))
