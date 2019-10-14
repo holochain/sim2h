8b8dc3fa99b64cfde492dda77c0442b17885a8c0
Nicolas Luck <nicolas@lucksus.eu>
2019-10-15 00:40:04 +0200

    Add MessageLogger interface

---
+++ crates/sim2h/src/message_log.rs
@@ -0,0 +1,53 @@
+use crate::WireMessage;
+use std::collections::LinkedList;
+use lib3h_protocol::uri::Lib3hUri;
+use lib3h_protocol::Address;
+use parking_lot::Mutex;
+
+#[derive(Serialize)]
+enum Direction {
+    In,
+    Out,
+}
+
+#[derive(Serialize)]
+struct MessageLog {
+    uri: Lib3hUri,
+    agent: Address,
+    direction: Direction,
+    message: WireMessage
+}
+
+lazy_static! {
+    pub static ref MESSAGE_LOGGER: Mutex<MessageLogger> = Mutex::new(MessageLogger::new());
+}
+
+pub struct MessageLogger {
+    buffer: LinkedList<MessageLog>,
+}
+
+impl MessageLogger {
+    pub fn new() -> Self {
+        MessageLogger {
+            buffer: LinkedList::new(),
+        }
+    }
+
+    pub fn log_in(&mut self, agent: Address, uri: Lib3hUri, message: WireMessage) {
+        self.buffer.push_back(MessageLog {
+            uri,
+            agent,
+            direction: Direction::In,
+            message,
+        });
+    }
+
+    pub fn log_out(&mut self, agent: Address, uri: Lib3hUri, message: WireMessage) {
+        self.buffer.push_back(MessageLog {
+            uri,
+            agent,
+            direction: Direction::Out,
+            message,
+        });
+    }
+}
