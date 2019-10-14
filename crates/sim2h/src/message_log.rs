9aa4a80cde798d0e9c2c8bff461548a6236539ac
Nicolas Luck <nicolas@lucksus.eu>
2019-10-15 01:06:13 +0200

    Log output thread

--- crates/sim2h/src/message_log.rs
+++ crates/sim2h/src/message_log.rs
@@ -3,6 +3,9 @@ use lib3h_protocol::uri::Lib3hUri;
 use lib3h_protocol::Address;
 use parking_lot::Mutex;
 use std::collections::LinkedList;
+use std::fs::OpenOptions;
+use std::io::Write;
+use log::error;
 
 #[derive(Serialize)]
 enum Direction {
@@ -19,7 +22,10 @@ struct MessageLog {
 }
 
 lazy_static! {
-    pub static ref MESSAGE_LOGGER: Mutex<MessageLogger> = Mutex::new(MessageLogger::new());
+    pub static ref MESSAGE_LOGGER: Mutex<MessageLogger> = {
+        MessageLogger::write_thread();
+        Mutex::new(MessageLogger::new())
+    };
 }
 
 pub struct MessageLogger {
@@ -31,6 +37,34 @@ impl MessageLogger {
         MessageLogger {
             buffer: LinkedList::new(),
         }
+    }
+
+    fn write_thread() {
+        std::thread::Builder::new().name("MessageLogger".into()).spawn(|| {
+            loop {
+                std::thread::sleep(std::time::Duration::from_secs(1));
+                if let Ok(mut file) = OpenOptions::new()
+                    .append(true)
+                    .open("sim2h_message_log.txt")
+                {
+                    let to_append = MESSAGE_LOGGER.lock()
+                        .buffer
+                        .split_off(0)
+                        .into_iter()
+                        //.drain_filter(|_| true)
+                        .map(|log| {
+                            serde_json::to_string(&log).expect("MessageLogs must be serializable")
+                        })
+                        .collect::<Vec<String>>()
+                        .join(",\n");
+                    if let Err(e) = file.write(to_append.as_bytes()) {
+                        error!("Error writing log file: {:?}", e);
+                    }
+                } else {
+                    error!("Could not open log file!")
+                }
+            }
+        }).expect("Could not spaw logger thread");
     }
 
     pub fn log_in(&mut self, agent: Address, uri: Lib3hUri, message: WireMessage) {
